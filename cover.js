17116205b064034398f4fb639f0c4ce204a074f6
Niklas Tegnander <bipshark@gmail.com>
2015-07-10 10:20:59 +0200

    Add image fetching logic

--- cover.js
+++ cover.js
@@ -1,2 +1,18 @@
+(function (window, document, undefined) {
+  var url = 'https://api.youpic.com/web_image';
 
+  var xhr = new XMLHttpRequest();
 
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      var result = JSON.parse(xhr.responseText);
+
+      if (result && result.image_urls) {
+        document.querySelector(".bg-photo").style.backgroundImage = 'url(' + result.image_urls.huge + ')';
+      }
+    }
+  }
+
+  xhr.open("GET", url, true);
+  xhr.send();
+})(window, document);
