90f1fe91531c19e9ee7aa82fd1ec1bc3ea0d84b8
Philip Laine <philip.laine@gmail.com>
2015-07-15 17:54:32 +0200

    Fix profile image loading bug

--- cover.js
+++ cover.js
@@ -4,29 +4,23 @@
  */
 (function (window, document, undefined) {
 
-  var updateTab = function (image_url, username, display_name, profile_image) {
+  var updateTab = function (data) {
     var bg = document.querySelector('.bg-photo');
-    bg.style.backgroundImage = 'url(' + image_url + ')';
-
-    // Fallback if no display name is set
-    display_name = display_name === '' ? username : display_name;
+    bg.style.backgroundImage = 'url(' + data.image + ')';
 
     var author_link = document.querySelector('.author a');
-    author_link.href = 'https://youpic.com/' + username;
-    author_link.innerHTML = display_name + '<span class="profile"></span>'
+    author_link.href = data.user_link;
+    author_link.innerHTML = data.display_name + '<span class="profile"></span>'
 
     var profile = document.querySelector('.profile');
-    profile.style.backgroundImage = 'url(' + profile_image + ')';
+    profile.style.backgroundImage = 'url(' + data.profile_image + ')';
   };
 
   // Fetch from cache
-  chrome.storage.sync.get('last_result', function (result) {
-    if (!result.last_result) return;
+  chrome.storage.sync.get('data', function (result) {
+    if (!result.data) return;
 
-    updateTab(result.last_result.image_urls.huge,
-      result.last_result.user.username,
-      result.last_result.user.display_name,
-      result.last_result.user.profile_image_urls.small);
+    updateTab(result.data);
   });
 
   var url = 'https://api.youpic.com/web_image', xhr = new XMLHttpRequest();
@@ -36,11 +30,21 @@
       var result = JSON.parse(xhr.responseText);
 
       if (result && result.image_urls && result.user) {
-        updateTab(result.image_urls.huge, result.user.username,
-          result.user.display_name, result.user.profile_image_urls.small);
+        var profile_image = (result.user && result.user.profile_image_urls
+          && result.user.profile_image_urls.small) 
+          || 'https://youpic.com/images/avatar/avatar-32.png';
+
+        var data = {
+          user_link: 'https://youpic.com/' + result.user.username, 
+          display_name: result.user.display_name,
+          image: result.image_urls.huge,
+          profile_image: profile_image
+        }
+
+        updateTab(data); // Display image
 
         // Update cache
-        chrome.storage.sync.set({ 'last_result': result }); // Omit callback
+        chrome.storage.sync.set({ 'data': data }); // Omit callback
       }
     }
   };
